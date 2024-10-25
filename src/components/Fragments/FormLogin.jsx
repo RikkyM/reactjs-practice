786415f91d111c980dbb322e7d28a3e32dcd144e
RikkyM <rikky.mahendra54@gmail.com>
2024-10-25 13:18:03 +0700

    useEffect

---
+++ src/components/Fragments/FormLogin.jsx
@@ -0,0 +1,33 @@
+import Button from "../Elements/Button";
+import InputForm from "../Elements/Input";
+
+const FormLogin = (props) => {
+  const handleLogin = (e) => {
+    e.preventDefault();
+    localStorage.setItem("email", e.target.email.value);
+    localStorage.setItem("password", e.target.password.value);
+    window.location.href = "/products";
+  };
+
+  return (
+    <form onSubmit={handleLogin}>
+      <InputForm
+        label="Email"
+        type="email"
+        name="email"
+        placeholder="example@mail.com"
+      />
+      <InputForm
+        label="Password"
+        type="password"
+        name="password"
+        placeholder="*****"
+      />
+      <Button classname="bg-blue-600 w-full" type="submit">
+        Login
+      </Button>
+    </form>
+  );
+};
+
+export default FormLogin;
